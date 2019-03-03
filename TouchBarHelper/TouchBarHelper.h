9c024b55bda7d63840f713275f2936ee1f3d5ef1
Pauli Jokela <pauli.jokela@didstopia.com>
2019-03-03 07:54:31 +0200

    Initial Commit

---
+++ TouchBarHelper/TouchBarHelper.h
@@ -0,0 +1,19 @@
+//
+//  TouchBarHelper.h
+//  TouchBarHelper
+//
+//  Created by Pauli Jokela on 03/03/2019.
+//  Copyright © 2019 Didstopia. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TouchBarHelper.
+FOUNDATION_EXPORT double TouchBarHelperVersionNumber;
+
+//! Project version string for TouchBarHelper.
+FOUNDATION_EXPORT const unsigned char TouchBarHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TouchBarHelper/PublicHeader.h>
+
+
